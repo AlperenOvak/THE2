e044229759bbd109518e9219de5547ae520778e6
AlperenOvak <alperen.ovak@metu.edu.tr>
2022-11-20 22:31:52 +0300

    related to line

--- THE2.py
+++ THE2.py
@@ -9,7 +9,7 @@ middle34=[(coor_list[2][0]+coor_list[3][0])/2,(coor_list[2][1]+coor_list[3][1])/
 #middle41=[(coor_list[3][0]+coor_list[0][0])/2,(coor_list[3][1]+coor_list[0][1])/2]
 area_quad=abs(((middle12[0]*middle23[1])+(middle23[0]*middle34[1])+(middle34[0]*middle12[1]))-((middle12[1]*middle23[0])+(middle23[1]*middle34[0])+(middle34[1]*middle12[0])))*2
 
-#print(area_quad)a
+#print(area_quad)
 
 #detect the min x and min_index
 if coor_list[0][0]<coor_list[1][0] and coor_list[0][0]<coor_list[2][0] and coor_list[0][0]<coor_list[3][0]:
@@ -88,10 +88,10 @@ m=(x_max[1]-x_min[1])/(x_max[0]-x_min[0])
 #if coor_list[indexs[0]][1]-coor_list[x_max_index][1]>m(coor_list[indexs[0]][0]-coor_list[x_max_index][0]) and coor_list[indexs[1]][1]-coor_list[x_max_index][1]>m(coor_list[indexs[1]][0]-coor_list[x_max_index][0]):
 
 
-
-if coor_list[indexs[0]][1]>coor_list[x_max_index][1] and coor_list[indexs[0]][1]>coor_list[x_min_index][1] and coor_list[indexs[1]][1]>coor_list[x_max_index][1] and coor_list[indexs[1]][1]>coor_list[x_min_index][1]:
+#both higher than line
+if coor_list[indexs[0]][1]-coor_list[x_max_index][1]>m*(coor_list[indexs[0]][0]-coor_list[x_max_index][0]) and coor_list[indexs[1]][1]-coor_list[x_max_index][1]>m*(coor_list[indexs[1]][0]-coor_list[x_max_index][0]):
     bottom_area=(abs(coor_list[x_max_index][1])+abs(coor_list[x_min_index][1]))*(coor_list[x_max_index][0]-coor_list[x_min_index][0])/2
-    if abs(x_max_index-x_min_index)!=1 or abs(x_max_index-x_min_index)!=3:
+    if abs(x_max_index-x_min_index)!=1 and abs(x_max_index-x_min_index)!=3:
         if coor_list[indexs[0]][1]<coor_list[indexs[1]][1]:
             bottom_point=coor_list[indexs[0]]
         else:
@@ -99,7 +99,9 @@ if coor_list[indexs[0]][1]>coor_list[x_max_index][1] and coor_list[indexs[0]][1]
         triangle=abs(((coor_list[x_max_index][0]*coor_list[x_min_index][1])+(coor_list[x_min_index][0]*bottom_point[1])+(bottom_point[0]*coor_list[x_max_index][1]))-((coor_list[x_max_index][1]*coor_list[x_min_index][0])+(coor_list[x_min_index][1]*bottom_point[0])+(bottom_point[1]*coor_list[x_max_index][0])))*2
         bottom_area+=triangle
     print(bottom_area)    
-elif coor_list[indexs[0]][1]<coor_list[x_max_index][1] and coor_list[indexs[0]][1]<coor_list[x_min_index][1] and coor_list[indexs[1]][1]<coor_list[x_max_index][1] and coor_list[indexs[1]][1]<coor_list[x_min_index][1]:
+
+#both lower than line
+elif coor_list[indexs[0]][1]-coor_list[x_max_index][1]<m*(coor_list[indexs[0]][0]-coor_list[x_max_index][0]) and coor_list[indexs[1]][1]-coor_list[x_max_index][1]<m*(coor_list[indexs[1]][0]-coor_list[x_max_index][0]):
     bottom_area=(abs(coor_list[x_max_index][1])+abs(coor_list[x_min_index][1]))*(coor_list[x_max_index][0]-coor_list[x_min_index][0])/2
     if abs(x_max_index-x_min_index)!=1 and abs(x_max_index-x_min_index)!=3:
         if coor_list[indexs[0]][1]>coor_list[indexs[1]][1]:

d0d92f62834e623eae07d5ff224c92e13092ef68
AlperenOvak <alperen.ovak@metu.edu.tr>
2022-11-26 19:34:01 +0300

    Final short

--- THE2-1.py
+++ THE2-1.py
@@ -5,63 +5,44 @@ middle23=[(coor_list[1][0]+coor_list[2][0])/2,(coor_list[1][1]+coor_list[2][1])/
 middle34=[(coor_list[2][0]+coor_list[3][0])/2,(coor_list[2][1]+coor_list[3][1])/2]
 area_quad=abs(((middle12[0]*middle23[1])+(middle23[0]*middle34[1])+(middle34[0]*middle12[1]))-((middle12[1]*middle23[0])+(middle23[1]*middle34[0])+(middle34[1]*middle12[0])))*2
 
-min_list=[]
-max_list=[]
+max_list,min_list=[],[]
 #detect the min x and min_index
 if coor_list[0][0]<=coor_list[1][0] and coor_list[0][0]<=coor_list[2][0] and coor_list[0][0]<=coor_list[3][0]:
-    min_list.append(coor_list[0])
     min_list.append(0)
 if coor_list[1][0]<=coor_list[0][0] and coor_list[1][0]<=coor_list[2][0] and coor_list[1][0]<=coor_list[3][0]:
-    min_list.append(coor_list[1])
     min_list.append(1)
 if coor_list[2][0]<=coor_list[0][0] and coor_list[2][0]<=coor_list[1][0] and coor_list[2][0]<=coor_list[3][0]:
-    min_list.append(coor_list[2])
     min_list.append(2)
 if coor_list[3][0]<=coor_list[0][0] and coor_list[3][0]<=coor_list[1][0] and coor_list[3][0]<=coor_list[2][0]:
-    min_list.append(coor_list[3])
     min_list.append(3)
 
-if len(min_list)==4:
-    if min_list[0][1]>min_list[2][1]:
-        x_min=min_list[2]
-        x_min_index=min_list[3]
-    else:
-        x_min=min_list[0]
+if len(min_list)==2:
+    if coor_list[min_list[0]][1]>coor_list[min_list[1]][1]:
         x_min_index=min_list[1]
 else:
-    x_min=min_list[0]
-    x_min_index=min_list[1]
+    x_min_index=min_list[0]
 #detect the max x
 if coor_list[0][0]>=coor_list[1][0] and coor_list[0][0]>=coor_list[2][0] and coor_list[0][0]>=coor_list[3][0]:
-    max_list.append(coor_list[0])
     max_list.append(0)
 if coor_list[1][0]>=coor_list[0][0] and coor_list[1][0]>=coor_list[2][0] and coor_list[1][0]>=coor_list[3][0]:
-    max_list.append(coor_list[1])
     max_list.append(1)
 if coor_list[2][0]>=coor_list[0][0] and coor_list[2][0]>=coor_list[1][0] and coor_list[2][0]>=coor_list[3][0]:
-    max_list.append(coor_list[2])
     max_list.append(2)
 if coor_list[3][0]>=coor_list[0][0] and coor_list[3][0]>=coor_list[1][0] and coor_list[3][0]>=coor_list[2][0]:
-    max_list.append(coor_list[3])
     max_list.append(3)
 
-if len(max_list)==4:
-    if max_list[0][1]>max_list[2][1]:
-        x_max=max_list[0]
+if len(max_list)==2:
+    if coor_list[max_list[0]][1]>coor_list[max_list[1]][1]:
         x_max_index=max_list[1]
-    else:
-        x_max=max_list[2]
-        x_max_index=max_list[3]
 else:
-    x_max=max_list[0]
-    x_max_index=max_list[1]
+    x_max_index=max_list[0]
 
 indexs=[0,1,2,3]
-indexs.remove(x_max_index)
+indexs.remove(x_max_index) 
 indexs.remove(x_min_index)
 
 #equation of the line
-m=(abs(x_max[1])-abs(x_min[1]))/(x_max[0]-x_min[0])
+m=(abs(coor_list[x_max_index][1])-abs(coor_list[x_min_index][1]))/(coor_list[x_max_index][1]-coor_list[x_min_index][0])
 bottom_area=(abs(coor_list[x_max_index][1])+abs(coor_list[x_min_index][1]))*abs(coor_list[x_max_index][0]-coor_list[x_min_index][0])/2
 
 #both higher than line
